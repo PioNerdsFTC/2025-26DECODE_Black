e5e1b39d1eecfcc39fcc5bd77606d369c6af7676
bob awesome <bobisawesome07@outlook.com>
2025-09-09 15:29:23 -0500

    Fix indentation and formatting in Bot class definition in bot.java

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/bot.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/bot.java
@@ -1,11 +1,8 @@
 package org.pionerds.ftc.teamcode;
 
 public class Bot {
-
-public class Bot {
     public static void main(String[] args) {
         System.out.println("Hello, PionerDS!");
     }
-}
 
 }
