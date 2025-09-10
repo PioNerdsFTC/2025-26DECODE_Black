9e337fa9397e537379a0e4dbc1199ec598bfc29f
LuketheFierce <lucas.schwietz@gmail.com>
2025-09-10 16:23:12 -0500

    first test of enum code

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Bot.java
@@ -2,6 +2,7 @@ package org.pionerds.ftc.teamcode;
 
 public class Bot {
     public static void main(String[] args) {
-        System.out.println("Hello, PionerDS!");
+        System.out.println("Hello, PioneerDS!");
+        Artifact ball1 = Artifact.GREEN;
     }
 }
