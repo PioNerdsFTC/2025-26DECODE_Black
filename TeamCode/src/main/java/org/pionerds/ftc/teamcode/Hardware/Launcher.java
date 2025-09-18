443b1674be779992ea9524d60a3ffdda80279315
nuclearcoremeltdown <johnathan.harrington@hmstudents.org>
2025-09-18 16:11:43 -0500

    changed power to velocity, able to change velocity with x (decrease) and b (increase)

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Launcher.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Launcher.java
@@ -1,14 +1,15 @@
 package org.pionerds.ftc.teamcode.Hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class Launcher {
     Hardware hardware = null;
 
-    public DcMotor launcher0;
-    public DcMotor launcher1;
+    public DcMotorEx launcher0;
+    public DcMotorEx launcher1;
 
     Launcher() {
 
@@ -17,21 +18,30 @@ public class Launcher {
     public void init(Hardware hardware) {
         this.hardware = hardware;
 
-        launcher0 = this.hardware.mapping.getMotor("launcher0", 3.0, DcMotorSimple.Direction.FORWARD, DcMotor.ZeroPowerBehavior.FLOAT);
-        launcher1 = this.hardware.mapping.getMotor("launcher1", 3.0, DcMotorSimple.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT);
+        launcher0 = this.hardware.mapping.getMotor("launcher0", 3.0, DcMotorSimple.Direction.FORWARD, DcMotorEx.ZeroPowerBehavior.FLOAT);
+        launcher1 = this.hardware.mapping.getMotor("launcher1", 3.0, DcMotorSimple.Direction.REVERSE, DcMotorEx.ZeroPowerBehavior.FLOAT);
     }
 
-    public void setLauncherPower(double power) {
-        launcher0.setPower(power);
-        launcher1.setPower(power);
+    public void setLauncherVelocity(double velocity) {
+        launcher0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher0.setVelocity(velocity);
+        launcher1.setVelocity(velocity);
     }
 
     public void launcherButton(Gamepad operatorGamepad) {
+        double currentVelocity = 6000;
         if (operatorGamepad.y) {
-            setLauncherPower(1);
+            setLauncherVelocity(currentVelocity);
         }
         else {
-            setLauncherPower(0);
+            setLauncherVelocity(0);
+        }
+        if (operatorGamepad.x) {
+            setLauncherVelocity(currentVelocity-100);
+        }
+        if (operatorGamepad.b) {
+            setLauncherVelocity(currentVelocity+100);
         }
     }
 }
