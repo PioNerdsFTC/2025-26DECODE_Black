608af033e868615dedb10ddfb11e0cd6092057b3
nuclearcoremeltdown <johnathan.harrington@hmstudents.org>
2025-09-25 15:46:00 -0500

    coded the storage class, almost ready for testing

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Hardware.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Hardware.java
@@ -15,6 +15,7 @@ final public class Hardware {
     public Mapping mapping = new Mapping();
     public Vision vision = new Vision();
     public Launcher launcher = new Launcher();
+    public Storage storage = new Storage();
 
 
     private Telemetry telemetry = null;
@@ -33,6 +34,7 @@ final public class Hardware {
             drivetrain.init(this);
             vision.init(this);
             launcher.init(this);
+            storage.init(this);
         } catch(Exception e) {
             telemetry.addLine(e.getMessage());
             this.continueRunning = false;
