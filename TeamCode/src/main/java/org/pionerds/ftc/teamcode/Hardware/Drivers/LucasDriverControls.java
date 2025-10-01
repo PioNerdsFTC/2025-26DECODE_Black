44d038f8da0196f19108ea4d2c2fa446318c985f
Lucas Schwietz <lucas.schwietz@gmail.com>
2025-09-30 19:55:45 -0500

    Added the abstract DriverControls.java class, which allows you to make custom Gamepad Controls for the TeleOps. Do this by making a new control class that extends the DriverControls.java class. Next, @Override the tickControls() method and add your controls inside. Finally, you can instantiate your new Driver in the TeleOp and pass is as a DriverControls.
    Also, updated the Hardware.java to support the new DriverControls changes.

---
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Drivers/LucasDriverControls.java
@@ -0,0 +1,64 @@
+package org.pionerds.ftc.teamcode.Hardware.Drivers;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.pionerds.ftc.teamcode.Hardware.AprilTagNames;
+import org.pionerds.ftc.teamcode.Hardware.Hardware;
+import org.pionerds.ftc.teamcode.Hardware.PioNerdAprilTag;
+
+public class LucasDriverControls extends DriverControls {
+    public LucasDriverControls(String driverName, float maxSpeed){
+        super(driverName,maxSpeed);
+    }
+
+    /**
+     * Ticked every loop in the TeleOp.
+     * @param gamepad
+     * @param hardware
+     *
+     * @Controls:
+     * Left_Bumper - 0.5 Speed Modifier <br>
+     * A_Button - Feed Intake <br>
+     * !(A_Button) - Contract Intake <br>
+     * X_Button - Send PioNerdTag distance to Aimbot <br>
+     * Right_Trigger - Set Launcher Velocity * 400 (@Overrides X_Button) <br>
+     * Left_Stick - Sends Positional Request to Drivetrain <br>
+     * Right_Stick.x - Sends Rotational Request to Drivetrain
+     *
+     **/
+    @Override
+    public void tickControls(Gamepad gamepad, Hardware hardware) {
+        // Resets
+        speedMultiplier = 1.0f;
+
+        // Left Bumper
+        if(gamepad.left_bumper){
+            speedMultiplier = 0.5f;
+            maxRotationSpeed = 0.5f;
+        }
+
+        // A-Button
+        if(gamepad.a){
+            hardware.storage.feed();
+        } else {
+            hardware.storage.contract();
+        }
+
+        PioNerdAprilTag blueTarget = hardware.vision.getPioNerdAprilTag(AprilTagNames.BlueTarget);
+        if (gamepad.x && !(blueTarget == null) && gamepad.right_trigger > 0) {
+            // Send the distance to the aimbot class
+            hardware.launcher.setLauncherVelocity(blueTarget.range(2));
+        }
+        if (gamepad.right_trigger > 0) {
+            hardware.launcher.setLauncherVelocity(gamepad.right_trigger*400);
+        }
+
+        // Set Rotation Speed for Drivetrain
+        rotationSpeed = Math.min(gamepad.right_stick_x,maxRotationSpeed);
+
+        // Set Speeds to the value or the capped value for the driver
+        speedX = Math.min(gamepad.left_stick_x,maxSpeed)*speedMultiplier;
+        speedY = Math.min(gamepad.left_stick_y,maxSpeed)*speedMultiplier;
+
+    }
+}
