14fafecfb23936bdb3bba25261d8291001094895
bob awesome <bobisawesome07@outlook.com>
2025-10-06 10:19:40 -0500

    Add pathfinding classes for robot localization and drawing utilities

---
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Pathfinding/Tuning/ManualFeedbackTuner.java
@@ -0,0 +1,29 @@
+package org.pionerds.ftc.teamcode.Pathfinding.Tuning;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pionerds.ftc.teamcode.Pathfinding.RoadRunnerUtils.MecanumDrive;
+
+public final class ManualFeedbackTuner extends LinearOpMode {
+    public static double DISTANCE = 64;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+            waitForStart();
+
+            while (opModeIsActive()) {
+                Actions.runBlocking(
+                    drive.actionBuilder(new Pose2d(0, 0, 0))
+                            .lineToX(DISTANCE)
+                            .lineToX(0)
+                            .build());
+            }
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}
