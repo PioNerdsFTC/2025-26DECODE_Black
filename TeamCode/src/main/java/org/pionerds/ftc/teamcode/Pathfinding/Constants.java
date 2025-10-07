85e2fd9420a8d7035548f50bed5d8da57ddd30d5
bob awesome <bobisawesome07@outlook.com>
2025-10-07 10:16:33 -0500

    Add DriveEncoderConstants for localizer configuration

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Pathfinding/Constants.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Pathfinding/Constants.java
@@ -4,6 +4,8 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -23,12 +25,25 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .robotWidth(0)
+            .robotLength(0)
+            .rightFrontMotorName("motor0")
+            .rightRearMotorName("motor3")
+            .leftRearMotorName("motor2")
+            .leftFrontMotorName("motor1")
+            .leftFrontEncoderDirection(Encoder.REVERSE)
+            .leftRearEncoderDirection(Encoder.REVERSE)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD);
+
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .driveEncoderLocalizer(localizerConstants)
                 .build();
     }
 }
