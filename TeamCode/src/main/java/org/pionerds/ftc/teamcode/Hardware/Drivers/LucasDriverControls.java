a322d6c7f78aed536b188fbb36f64d03816d50c1
Lucas Schwietz <lucas.schwietz@gmail.com>
2025-10-29 09:20:55 -0500

    fix duplicate variable

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Drivers/LucasDriverControls.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Drivers/LucasDriverControls.java
@@ -30,7 +30,6 @@ public class LucasDriverControls extends DriverControls {
      * Right_Stick.x - Sends Rotational Request to Drivetrain
      *
      **/
-    boolean start_pressed_already = false;
     boolean reset_Gyro_Pressed = false;
 
     @Override
