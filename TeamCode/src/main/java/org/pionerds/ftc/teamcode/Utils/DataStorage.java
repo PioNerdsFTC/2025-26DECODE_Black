c248f36148c79b813f2186322619eec572813ec5
FantasticalFish <ryan.comer@hmstudents.org>
2025-11-05 15:45:51 -0600

    Added DataStorage Class with storing and fetching methods for static variable: "angles" (gyro data)

---
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Utils/DataStorage.java
@@ -0,0 +1,25 @@
+package org.pionerds.ftc.teamcode.Utils;
+
+public class DataStorage {
+
+    static double[] angles = {0.0,0.,0.0}; //yaw, pitch, roll
+
+    public static double getStoredAngle(int index) {
+        return angles[index];
+    }
+
+    public static void storeAngle(int index, double ang) {
+        angles[index] = ang;
+    }
+
+    public static double[] getAllStoredAngles() {
+        return angles;
+    }
+
+    public static void storeAllAngles(double[] angs) {
+        angles[0] = angs[0];
+        angles[1] = angs[1];
+        angles[2] = angs[2];
+    }
+
+}
