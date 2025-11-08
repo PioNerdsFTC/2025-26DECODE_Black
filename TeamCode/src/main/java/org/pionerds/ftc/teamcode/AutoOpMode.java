f15c5d5a3ac1051e38f5ae8a5fb506d3f569adca
bob awesome <bobisawesome07@outlook.com>
2025-11-08 09:49:58 -0600

    refactor: enhance pathfinding logic and update artifact scanning mechanism

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/AutoOpMode.java
@@ -10,7 +10,6 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.pionerds.ftc.teamcode.Hardware.Artifact;
 import org.pionerds.ftc.teamcode.Hardware.Hardware;
 import org.pionerds.ftc.teamcode.Pathfinding.Constants;
 
@@ -25,13 +24,23 @@ public class AutoOpMode extends OpMode {
     private final Pose startPose = new Pose(56, Constants.localizerConstants.robot_Width/2, Math.toRadians(90)); // Start Pose of our robot.
     private final Pose scanPose = new Pose(56, 80, Math.toRadians(90));
     private final Pose scorePose = new Pose(48, 110, Math.toRadians(144.046));
-    private Artifact[] artifactPattern = new Artifact[3];
+    private Pose pickupPose = new Pose(48, 84, Math.toRadians(180));
+    private final Pose endPose = new Pose(38.75, 33.25, Math.toRadians(180));
+
+    private boolean scanned = false;
+    private int pickupCycle = 0;
+
+    private String artifactPattern = new String("No scan attempt yet");
+
     final Hardware hardware = new Hardware();
 
     private int pathState;
 
-    public PathBuilder pathBuilder;
-    public static PathChain startToScoreChain;
+    private PathBuilder pathBuilder;
+    private PathChain startToScoreChain;
+    private PathChain scoreToPickupChain;
+    private PathChain pickupToScoreChain;
+
 
     /**
      * These change the states of the paths and actions. It will also reset the timers of the individual switches
@@ -46,16 +55,18 @@ public class AutoOpMode extends OpMode {
      **/
     @Override
     public void loop() {
-        follower.update();
+        if(!scanned){
+            artifactPattern = Arrays.toString(hardware.vision.getArtifactPattern());
+        }
         autonomousPathUpdate();
         // These loop the movements of the robot, these must be called continuously in order to work
-
+        follower.update();
         // Feedback to Driver Hub for debugging
         telemetry.addData("path state", pathState);
         telemetry.addData("x", follower.getPose().getX());
         telemetry.addData("y", follower.getPose().getY());
         telemetry.addData("heading", follower.getPose().getHeading());
-        telemetry.addData("pattern", Arrays.toString(artifactPattern));
+        telemetry.addData("pattern", artifactPattern);
         telemetry.update();
     }
 
@@ -73,12 +84,23 @@ public class AutoOpMode extends OpMode {
         follower.setStartingPose(startPose);
 
         hardware.init(hardwareMap, telemetry, hardware.elapsedTime);
+
         startToScoreChain = pathBuilder
             .addPath(new BezierLine(startPose, scanPose))
             .setConstantHeadingInterpolation(Math.toRadians(90))
-            .addPoseCallback(scanPose, ()->{hardware.vision.getArtifactPattern();}, 0.5)
             .addPath(new BezierCurve(scanPose, scorePose))
-            .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(144.046))
+            .addParametricCallback(80, () -> {scanned=true;})
+            .setLinearHeadingInterpolation(scanPose.getHeading(), scorePose.getHeading())
+            .build();
+
+        scoreToPickupChain = pathBuilder
+            .addPath(new BezierCurve(scorePose, pickupPose))
+            .setLinearHeadingInterpolation(scorePose.getHeading(), pickupPose.getHeading())
+            .build();
+
+        pickupToScoreChain = pathBuilder
+            .addPath(new BezierCurve(pickupPose, scorePose))
+            .setLinearHeadingInterpolation(pickupPose.getHeading(), scorePose.getHeading())
             .build();
     }
 
@@ -114,11 +136,22 @@ public class AutoOpMode extends OpMode {
 
             case 1:
                 if(!follower.isBusy()) {
+                    pickupPose = pickupPose.withY(pickupPose.getY()-(24*pickupCycle));
+                    follower.followPath(scoreToPickupChain,false);
                     setPathState(2);
                 }
                 break;
 
-            case 87:
+            case 2:
+                if(pickupCycle==2 && !follower.isBusy()){
+                    setPathState(67);
+                }
+                else if(!follower.isBusy()){
+                    follower.followPath(pickupToScoreChain, false);
+                }
+                break;
+
+            case 67:
                 if(!follower.isBusy()) {
                     setPathState(-1);
                 }
