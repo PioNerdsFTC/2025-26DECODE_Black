9bb9e96734eab6a927d276e2e275f59a5090618e
bob awesome <bobisawesome07@outlook.com>
2025-11-22 10:48:38 -0600

    Refactor RaisingIt2 to use DcMotorEx array for improved motor management and set target positions

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/RaisingIt2.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/RaisingIt2.java
@@ -2,32 +2,46 @@ package org.pionerds.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
+import org.firstinspires.ftc.robotcore.external.navigation.VoltageUnit;
 import org.pionerds.ftc.teamcode.Hardware.Hardware;
 
 @Autonomous(name = "Raiser Auto")
 public class RaisingIt2 extends LinearOpMode {
 
     private final Hardware hardware = new Hardware();
-    private final String[] motorNames = {"motor0", "motor1", "motor2", "motor3"};
+
 
 
     @Override
     public void runOpMode() throws InterruptedException {
         hardware.init(hardwareMap, telemetry);
-
+        final DcMotorEx[] motors = {
+            hardwareMap.get(DcMotorEx.class, "motor0"),
+            hardwareMap.get(DcMotorEx.class, "motor1"),
+            hardwareMap.get(DcMotorEx.class, "motor2"),
+            hardwareMap.get(DcMotorEx.class, "motor3")
+        };
         telemetry.addLine("Robot initialized! (TeleOp)");
         telemetry.update();
 
         waitForStart();
 
-        for (String motorName : motorNames) {
-            hardwareMap.get(DcMotorEx.class, motorName).setMotorType();
+        for (DcMotorEx motor : motors) {
+            motor.setTargetPosition(3000);
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motor.setVelocity(1000);
+        }
 
+        while (motors[0].isBusy() || motors[1].isBusy() || motors[2].isBusy() || motors[3].isBusy()){
+            for (DcMotorEx motor : motors){
+                telemetry.addData(motor.getDeviceName() + " Position", motor.getCurrentPosition());
+            }
         }
 
         hardware.stop();
