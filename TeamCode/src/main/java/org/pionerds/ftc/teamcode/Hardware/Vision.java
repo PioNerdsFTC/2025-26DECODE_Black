d4ac702ca608639e1944b08f358b492d5743d962
Lucas Schwietz <lucas.schwietz@gmail.com>
2025-11-22 20:44:04 -0600

    fixed obelisk detection thingy

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Vision.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Vision.java
@@ -186,7 +186,6 @@ public class Vision {
             ArrayList<AprilTagMetadata> currentDetectionMetadata =
                 currentDetectionsMetadata();
 
-            obeliskIdentified = true;
 
             for (AprilTagMetadata metadata : currentDetectionMetadata) {
                 switch (metadata.name) {
@@ -194,18 +193,21 @@ public class Vision {
                         artifactAlgorithm[0] = Artifact.GREEN;
                         artifactAlgorithm[1] = Artifact.PURPLE;
                         artifactAlgorithm[2] = Artifact.PURPLE;
+                        obeliskIdentified = true;
 
                         return artifactAlgorithm;
                     case "Obelisk_PGP":
                         artifactAlgorithm[0] = Artifact.PURPLE;
                         artifactAlgorithm[1] = Artifact.GREEN;
                         artifactAlgorithm[2] = Artifact.PURPLE;
+                        obeliskIdentified = true;
 
                         return artifactAlgorithm;
                     case "Obelisk_PPG":
                         artifactAlgorithm[0] = Artifact.PURPLE;
                         artifactAlgorithm[1] = Artifact.PURPLE;
                         artifactAlgorithm[2] = Artifact.GREEN;
+                        obeliskIdentified = true;
 
                         return artifactAlgorithm;
                     default:
