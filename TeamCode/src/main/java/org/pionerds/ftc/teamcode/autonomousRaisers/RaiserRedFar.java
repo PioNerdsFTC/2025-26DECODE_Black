4e247e3670b40d74093055cadfeee8edb58b3c30
bob awesome <bobisawesome07@outlook.com>
2025-12-06 12:55:19 -0600

    gulp

---
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/autonomousRaisers/RaiserRedFar.java
@@ -0,0 +1,55 @@
+package org.pionerds.ftc.teamcode.autonomousRaisers;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.pionerds.ftc.teamcode.Hardware.AprilTagNames;
+import org.pionerds.ftc.teamcode.Hardware.Hardware;
+
+@Autonomous(name = "RaiserBlueFar")
+public class RaiserRedFar extends OpMode {
+
+    final Hardware hardware = new Hardware();
+    AprilTagNames target;
+
+    @Override
+    public void init() {
+        hardware.init(hardwareMap, telemetry);
+        telemetry.addLine("Robot initialized! (TeleOp)");
+        telemetry.update();
+
+        target = AprilTagNames.BlueTarget;
+        hardware.storage.resetEncoderSusan();
+
+    }
+
+    @Override
+    public void start() {
+        hardware.vision.getArtifactPattern();
+        ElapsedTime elapsedTime = new ElapsedTime();
+        hardware.addElapsedTime(elapsedTime);
+
+        telemetry.addLine("Robot runtime started! (TeleOp)");
+        telemetry.update();
+
+
+        // START AI CODE
+
+
+        hardware.raiser.driveByInches(96,0.3);
+
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+        hardware.stop();
+        super.stop();
+    }
+}
