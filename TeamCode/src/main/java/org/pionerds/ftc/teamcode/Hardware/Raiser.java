dba63d505e222a9161f32a6d97f59aba3ff6a797
Lucas Schwietz <lucas.schwietz@gmail.com>
2025-12-06 11:09:19 -0600

    fixed rounding in print logs again

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Raiser.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/Hardware/Raiser.java
@@ -80,18 +80,18 @@ public class Raiser {
             hardware.telemetry.addLine("\n");
 
             hardware.telemetry.addLine("Intended Driving Velocities:");
-            hardware.telemetry.addLine("Motor0: "+((int)(driveMotorDesiredVelocities[0]/10))*100);
-            hardware.telemetry.addLine("Motor1: "+((int)(driveMotorDesiredVelocities[1]/10))*100);
-            hardware.telemetry.addLine("Motor2: "+((int)(driveMotorDesiredVelocities[2]/10))*100);
-            hardware.telemetry.addLine("Motor3: "+((int)(driveMotorDesiredVelocities[3]/10))*100);
+            hardware.telemetry.addLine("Motor0: "+driveMotorDesiredVelocities[0]);
+            hardware.telemetry.addLine("Motor1: "+driveMotorDesiredVelocities[1]);
+            hardware.telemetry.addLine("Motor2: "+driveMotorDesiredVelocities[2]);
+            hardware.telemetry.addLine("Motor3: "+driveMotorDesiredVelocities[3]);
 
             hardware.telemetry.addLine("\n");
 
             hardware.telemetry.addLine("Set Driving Velocities:");
-            hardware.telemetry.addLine("Motor0: "+((int)(driveMotorVelocities[0])));
-            hardware.telemetry.addLine("Motor1: "+((int)(driveMotorVelocities[1])));
-            hardware.telemetry.addLine("Motor2: "+((int)(driveMotorVelocities[2])));
-            hardware.telemetry.addLine("Motor3: "+((int)(driveMotorVelocities[3])));
+            hardware.telemetry.addLine("Motor0: "+driveMotorVelocities[0]);
+            hardware.telemetry.addLine("Motor1: "+driveMotorVelocities[1]);
+            hardware.telemetry.addLine("Motor2: "+driveMotorVelocities[2]);
+            hardware.telemetry.addLine("Motor3: "+driveMotorVelocities[3]);
 
             hardware.telemetry.addLine("\nwaiting on motors for linear movement...");
             hardware.telemetry.addLine("waiting on motors for linear movement...");
