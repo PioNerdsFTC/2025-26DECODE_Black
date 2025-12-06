1ec2d3d5f85e8dc7aaa75be78a323f1b909c0019
Saintdoggie <saintdoggie@saintdoggie.org>
2025-12-06 12:56:18 -0600

    feat: add RaiserRedClose

---
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/autonomousRaisers/RaiserRedClose.java
@@ -0,0 +1,48 @@
+package org.pionerds.ftc.teamcode.autonomousRaisers;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.pionerds.ftc.teamcode.Hardware.AprilTagNames;
+import org.pionerds.ftc.teamcode.Hardware.Hardware;
+
+@Autonomous(name = "RaiserBlueFar")
+public class RaiserRedClose extends OpMode {
+
+    final Hardware hardware = new Hardware();
+    AprilTagNames target;
+
+    @Override
+    public void init() {
+        hardware.init(hardwareMap, telemetry);
+        telemetry.addLine("Robot initialized! (TeleOp)");
+        telemetry.update();
+
+        target = AprilTagNames.RedTarget;
+        hardware.storage.resetEncoderSusan();
+    }
+
+    @Override
+    public void start() {
+        hardware.vision.getArtifactPattern();
+        ElapsedTime elapsedTime = new ElapsedTime();
+        hardware.addElapsedTime(elapsedTime);
+
+        telemetry.addLine("Robot runtime started! (TeleOp)");
+        telemetry.update();
+
+        hardware.raiser.driveByInches(-24,-0.3);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+        hardware.stop();
+        super.stop();
+    }
+}
