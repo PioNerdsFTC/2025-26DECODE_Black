c9bf5729d6c103495550c5f0f190f74440390bea
Lucas Schwietz <lucas.schwietz@gmail.com>
2025-12-06 12:48:48 -0600

    new ops

--- TeamCode/src/main/java/org/pionerds/ftc/teamcode/RaiserAutoBlueClose.java
+++ TeamCode/src/main/java/org/pionerds/ftc/teamcode/RaiserAutoBlueClose.java
@@ -1,6 +1,7 @@
 package org.pionerds.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
